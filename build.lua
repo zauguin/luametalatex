560a0da74d091d9c2c94825e3d4c993aa2ba1c87
Marcel Fabian Krüger <zauguin@gmail.com>
2020-07-11 19:44:26 +0200

    Fix build.lua

--- build.lua
+++ build.lua
@@ -4,5 +4,5 @@ module = "luametalatex"
 
 tdsroot = "luametatex" -- Would be luametalatex but we use the same files for luametaplain
 
-installfiles = {"luametalatex*.lua", "luametaplain*.lua"}
-sourcefiles = {"*.lua"}
+installfiles = {"luametalatex*.lua", "luametaplain*.lua", "kpse.*"}
+sourcefiles = {"*.lua", "kpse.*"}
