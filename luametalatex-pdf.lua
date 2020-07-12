5262457cd87ad17f148a0c7d2bf64f99f98694be
Marcel Fabian Krüger <zauguin@gmail.com>
2020-07-12 02:15:08 +0200

    Use more meaningful magic bytes again
    The new sequence C1 AC C1 B4 is valid under Latin1 to get reasonable
    encoding autodetection but is a nonstandard UTF-8 encoding for "lt"

--- luametalatex-pdf.lua
+++ luametalatex-pdf.lua
@@ -166,7 +166,7 @@ local pdfmeta = {
 pdfmeta.__index = pdfmeta
 local function open(filename)
   local file = io.open(filename, 'wb')
-  file:write"%PDF-X.X\n%\xe4\xf6\xfc\xdf\n"
+  file:write"%PDF-X.X\n%\xC1\xAC\xC1\xB4\n"
   return setmetatable({file = file, version = '1.7', [0] = 0, pages = {}, objstream = {}}, pdfmeta)
 end
 return {
